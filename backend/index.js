6fdd2bcc86db7a6188d5f72b4c2a56f057ce078b
Quincy-Bee <quincybee@icloud.com>
2026-08-05 00:14:38 -0400

    schema for listings setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
 // route created for home
 app.get("/", (req, res) => {
-    res.send("Home Feed API Running");
+    res.json({
+        message: "HomeFeed API Running"
+    });
 });
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
-})
+});
+
---
+++ backend/models/Listings.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+// Schema for every listing
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  address: {
+    type: String,
+    required: true,
+  },
+
+  bedrooms: {
+    type: Number,
+    default: 0,
+  },
+
+  bathrooms: {
+    type: Number,
+    default: 0,
+  },
+
+  description: {
+    type: String,
+  },
+});
+
+// Create the Listing model
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
