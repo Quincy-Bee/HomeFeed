8761afcb6069da3574cd3e4a4c1b08a274cf020f
Quincy-Bee <quincybee@icloud.com>
2026-08-04 23:44:34 -0400

    frontend backend connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,32 +3,20 @@ import { useState, useEffect } from "react";
 function App() {
 
     // State for storing backend message
-
     const [message, setMessage] = useState("");
 
     // Fetch data from backend when component loads
-
     useEffect(() => {
-
         fetch("http://localhost:3000/")
-
             .then((response) => response.json())
-
             .then((data) => {
-
                 setMessage(data.message);
-
             });
-
     }, []);
 
+  return (
+    <h1>{message}</h1>
+  );
+};
 
-
-    return (
-
-        <h1>{message}</h1>
-
-    );
-
-}
 export default App;
