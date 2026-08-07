967f7d4a57385481626b3cd0ce50857cb80979bd
Quincy-Bee <quincybee@icloud.com>
2026-08-07 13:03:45 -0400

    log out  button made

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,64 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
+
 function Navbar() {
+
+    const navigate = useNavigate();
+
+
+    const logout = () => {
+
+        localStorage.removeItem("user");
+
+        navigate("/login");
+
+    };
+
+
     return (
+
         <nav className="navbar">
+
 
             <Link to="/">
                 Listings
             </Link>
 
+
             <Link to="/dashboard">
                 Dashboard
             </Link>
+
 
             <Link to="/dashboard/create">
                 Create Listing
             </Link>
 
+
             <Link to="/register">
                 Register
             </Link>
+
 
             <Link to="/login">
                 Login
             </Link>
 
+
+            <button
+                className="logout-button"
+                onClick={logout}
+            >
+                Logout
+            </button>
+
+
         </nav>
+
     );
+
 }
+
 
 export default Navbar;
