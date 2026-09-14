d117661e136526ca696959df87caf95792f79631
Quincy-Bee <quincybee@icloud.com>
2026-09-14 03:43:04 -0400

    testing123

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 import "./App.css";
 
@@ -20,6 +21,11 @@ function App() {
                 <Route
                     path="/login"
                     element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
                 />
             </Routes>
 
