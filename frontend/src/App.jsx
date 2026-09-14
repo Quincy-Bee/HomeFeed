138ce9d434908b2252a550f508301e0856be365d
Quincy-Bee <quincybee@icloud.com>
2026-09-14 03:41:39 -0400

    checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,80 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ListingDetails from "./pages/ListingDetails";
 import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
-import EditListing from "./pages/EditListing";
-import CreateListing from "./pages/CreateListing";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Footer from "./components/Footer";
-import NYCNeighborhoods from "./pages/NYCNeighborhoods";
-import FSBO from "./components/FSBO";
-//import "./App.css"; 
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+import "./App.css";
 
 function App() {
     return (
         <>
-
-        <Navbar />
+            <Navbar />
 
             <Routes>
-               
                 <Route
                     path="/"
                     element={<Home />}
                 />
 
                 <Route
-                    path="/nyc-neighborhoods"
-                    element={<NYCNeighborhoods />}
-                />
-
-                <Route
-                    path="/listings/:id"
-                    element={<ListingDetails />}
-                />
-
-                <Route
-                    path="/dashboard"
-                    element={
-                        <ProtectedRoute>
-                            <Dashboard />
-                        </ProtectedRoute>
-                    }
-                />
-
-                <Route
-                    path="/dashboard/edit/:id"
-                    element={
-                        <ProtectedRoute>
-                            <EditListing />
-                        </ProtectedRoute>
-                    }
-                />
-
-                <Route
-                    path="/dashboard/create"
-                    element={
-                        <ProtectedRoute>
-                            <CreateListing />
-                        </ProtectedRoute>
-                    }
-                />
-
-                <Route
-                    path="/register"
-                    element={<Register />}
-                />
-
-                <Route
                     path="/login"
                     element={<Login />}
                 />
-
             </Routes>
-            <FSBO />
 
             <Footer />
         </>
